0d52b90deeaf8d7a348582f71ffce46312a33dac
roucky <34217367+roucky44@users.noreply.github.com>
2026-06-15 16:43:00 +0200

    Updated the vite config with the package-lock.json set to peer : true

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./", // Force la racine au répertoire courant
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    fs: {
+      strict: true,
+      allow: ["."], // Autorise uniquement le dossier actuel
     },
   },
 });
